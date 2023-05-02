77b84ddd208fd44953c3610ccb80d1fcbaaf0d3f
joon jeong <juny.jeong@gmail.com>
2023-05-02 20:22:05 +1200

    adjust download cv function

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 export default function Home() {
   function handleDownLoad() {
-    fetch('../files/JOON-YOUNG-JEONG.pdf')
-      .then((res) => {
-        res
-          .blob()
-          .then((blob) => {
-            const fileURL = window.URL.createObjectURL(blob)
-            const alink = document.createElement('a')
-            alink.href = fileURL
-            alink.download = 'Joon-CV.pdf'
-            alink.click()
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    const fileName = 'cv.pdf'
+    const fileUrl = `${process.env.PUBLIC_URL}/files/${fileName}`
+
+    const link = document.createElement('a')
+    link.download = fileName
+    link.href = fileUrl
+    link.click()
+    // fetch('../files/JOON-YOUNG-JEONG.pdf')
+    //   .then((res) => {
+    //     res
+    //       .blob()
+    //       .then((blob) => {
+    //         const fileURL = window.URL.createObjectURL(blob)
+    //         const alink = document.createElement('a')
+    //         alink.href = fileURL
+    //         alink.download = 'Joon-CV.pdf'
+    //         alink.click()
+    //       })
+    //       .catch((error) => {
+    //         console.log(error)
+    //       })
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
   }
   return (
     <section id="homeSection">
