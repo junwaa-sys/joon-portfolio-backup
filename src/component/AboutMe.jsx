56b9cd61ad0b2d57cc4c068db5fd82788572f098
joon jeong <juny.jeong@gmail.com>
2023-05-03 05:32:22 +1200

    update about me

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -9,18 +9,30 @@ export default function AboutMe() {
         site.
       </p>
       <p>
-        Since 1990s, when I first learnt visual basic, my passion for tech,
-        especially software development has been{' '}
+        Although I have worked in the accounting industry for quite a long time,
+        since the 1990s
       </p>
       <p>
-        always with me despite I have worked in accounting industry for quite a
-        long time.
+        when I first learnt visual basic, my passion for tech, especially for
+        software development, has always been with me.
       </p>
       <br></br>
       <p>
-        Now, I returned where my passion is and this site will record my journey
-        as a software developer.
+        Now, after graduating Dev Academy, I finally returned to where my
+        passion is and this site will record my journey as a software developer.
       </p>
+      <p>
+        I am excited to seek opportunities to develop my career, learn new tech
+        from others and explore tech world.
+      </p>
+      <p>
+        So please feel free to contact me via email or linked In. I am excited
+        and eager to be connected with
+      </p>
+      <p>professionals or people with interested in tech like me.</p>
+      <br></br>
+      <p>Hope you enjoy watching my journey and contents of this site.</p>
+      <p>THANK YOU.!!!</p>
     </section>
   )
 }
