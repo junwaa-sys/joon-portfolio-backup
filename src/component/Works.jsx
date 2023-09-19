70053e279f5eeee630832d86c3ad31045bf59eab
joon jeong <juny.jeong@gmail.com>
2023-09-19 18:04:54 +1200

    add movie night component to main page.

--- src/component/Works.jsx
+++ src/component/Works.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Todo from './Todo'
 import PFO from './PFO'
+import MoiveNight from './MoiveNight'
 
 export default function Works() {
   return (
     <>
       <PFO />
       <Todo />
+      <MoiveNight />
     </>
   )
 }
